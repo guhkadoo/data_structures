5602247a1fb7decddba589b7ebd8d85bb3f57d3a
guhkadoo <gustavokadoo@hotmail.com>
2025-03-11 16:47:34 -0300

    fibonacci.h declares two functions: recursive fibonacci (uses memoization) and iterative fibonacci

---
+++ recursion/fibonacci/fibonacci.h
@@ -0,0 +1,7 @@
+#ifndef FIBONACCI_H
+#define FIBONACCI_H
+
+long long recursive_fibonacci(int n); //top-down approach using memoization
+long long iterative_fibonacci(int n);
+
+#endif //FIBONACCI_H
