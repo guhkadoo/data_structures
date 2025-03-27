caff40fadcc3b460d7b29fb74ce1745a5ad67289
guhkadoo <gustavokadoo@hotmail.com>
2025-03-27 11:46:41 -0300

    header with towers function

---
+++ recursion/hanoi_tower/towers.h
@@ -0,0 +1,10 @@
+#ifndef TOWERS_H
+#define TOWERS_H
+
+#include <stdlib.h>
+
+#define EXIT_WITH_ERROR(msg) (fprintf(stderr, "%s\n", msg), exit(1))
+
+void towers(int, char, char, char);
+
+#endif //TOWERS_H
